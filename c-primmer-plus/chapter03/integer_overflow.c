eddb4352334696757d19ea0e9993e384c8b386da
hualongfeiyyy@163.com <alberthua@kernel-as-efi.lab.example.com>
2023-08-02 13:56:04 +0800

    Update README.md

--- c-primmer-plus/chapter03/integer_overflow.c
+++ c-primmer-plus/chapter03/integer_overflow.c
@@ -12,16 +12,3 @@ int main(void) {
     /* 输出为：4294967295 0 1 */
 	return 0;
 }
-
-/* 各类整形变量的取值范围：
-     short, int (2^16, 16 bit): [-32768, 32767]
-     int, long (2^32, 32 bit): [-2147483648, 2147483647]
-     long long (2^64, 64 bit): [-9223372036854775808, 9223372036854775807]
-
-     unsigned short, int (2^16, 16 bit): [0, 65535]
-     unsigned int, long (2^32, 32 bit): [0, 4294967295]
-     unsigned long long (2^64, 64 bit): [0, 18446744073709551616]
-
-   C 语言编译器识别整数类型的顺序：
-     int, unsigned int, long, unsigned long, long long, unsigned long long  
- */
