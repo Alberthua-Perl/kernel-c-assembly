a8653fd2d9d03fd9e019524ea4a19c94d9390b46
hualongfeiyyy@163.com <alberthua@kernel-as-efi.lab.example.com>
2023-08-12 17:46:40 +0800

    Add praise2.c

--- c-primmer-plus/chapter04/praise1.c
+++ c-primmer-plus/chapter04/praise1.c
@@ -1,12 +1,19 @@
+/* praise1.c - 使用不同类型的字符串 */
 #include <stdio.h>
+#include <string.h>  /* 提供 strlen() 函数原型 */
 #define PRAISE "You are an extraordinary being."
 
 int main(void) {
     char name[40];
 
+    short length;
+    char s[6] = "";  /* 定义空字符串 */
+    length = strlen(s);  /* 实际字符串以 \0 结尾（占1字节）*/
+
     printf("What's your name? ");
     scanf("%s", name);
     printf("Hello, %s. %s\n", name, PRAISE);
+    printf("The length of s is %hd\n", length);
 
     return 0;
 }
---
+++ c-primmer-plus/chapter04/praise2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#define PRAISE "You are an extraordinary being."
+
+int main(void) {
+    char name[40];
+
+    printf("What's your name? ");
+    scanf("%s", name);
+    printf("Hello, %s. %s\n", name, PRAISE);
+    printf("Your name of %d letters occupies %zd memory cells.\n", 
+            strlen(name), sizeof name);
+    printf("The phrase of praise has %d letters ", 
+            strlen(PRAISE));
+    printf("and occupies %zd memory cells.\n", sizeof PRAISE);  /* 字符串常量末尾的空字符占1个字节 */ 
+
+    return 0;        
+}
