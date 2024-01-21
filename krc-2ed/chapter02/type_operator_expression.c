4010ed31d10b465e5897ac843228057053915579
Alberthua-Perl <hualongfeiyyy@163.com>
2024-01-21 21:18:11 +0800

    Update chapter02

--- krc-2ed/chapter02/type_operator_expression.c
+++ krc-2ed/chapter02/type_operator_expression.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
 
 //算术运算符
+int year;
+
+int main(void)
+{
+  printf("Please type year: ");
+  scanf("%d", &year);
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)  // 取模运算符（取余数）
+		printf("%d is a leap year.\n", year);
+	else
+		printf("%d is not a leap year.\n", year);
+
+  return 0;
+}
 
