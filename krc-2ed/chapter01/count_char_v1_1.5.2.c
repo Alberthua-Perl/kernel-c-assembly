e6ab102eb48600f226ce06ac5e17d336b5ef8e5b
Alberthua-Perl <hualongfeiyyy@163.com>
2024-03-29 16:52:02 +0800

    Update files in chapter01

--- krc-2ed/chapter01/count_char_v1_1.5.2.c
+++ krc-2ed/chapter01/count_char_v1_1.5.2.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
+/* 统计输入的字符数：版本1 */
 int main(void)
 {
     long nc = 0;
 
-    while (getchar() != EOF) {  // 输入至字符末尾处还包含换行符
+    while (getchar() != EOF) {  // 输入的字符中换行符也计算在内
         ++nc;
     }
     printf("char count is: %ld\n", nc);
